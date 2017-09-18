b9412f23b531381e2c1aa08b6734a92ff27b937b
Marie Alcantara <mcjalcantara@gmail.com>
2017-09-18 02:59:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -45,5 +45,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-
+  var newArray = array.slice(0, array.length - 1);
+  return newArray;
 }
