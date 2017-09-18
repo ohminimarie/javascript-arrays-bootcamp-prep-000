1d436d0883839f3a28ca04857ab73170ee069288
Marie Alcantara <mcjalcantara@gmail.com>
2017-09-18 02:51:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array = array.slice();
+  array.slice();
   return array;
 }
 
