d100acab4ee36a5e1e5096d9f15083fe1f440593
Marie Alcantara <mcjalcantara@gmail.com>
2017-09-18 02:49:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,12 +31,12 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
-  return array
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
-  var newArray = array.slice();
-  return newArray
+  array.slice();
+  return array;
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
